34b19edf98a706728e35d43afe8f9a0e2b36026f
JakubFrackowiak <xfragless@gmail.com>
2024-01-24 15:12:46 +0100

    first commit, add iconbutton and create toolbar

--- components/Toolbar/index.tsx
+++ components/Toolbar/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
-import { ToolbarIcon } from "./ToolbarIcon";
+import { ToolbarIcon } from "../ToolbarIcon";
 import { Stack } from "@mui/material";
 
 export function index() {
